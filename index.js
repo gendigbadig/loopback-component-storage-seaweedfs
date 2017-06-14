1a0229864c7ae64b29c291df9199cb9a7c71ce11
Rahmat Nugraha <rn.rahmatnugraha@gmail.com>
2017-06-14 15:19:15 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by gendigbadig on 6/14/17.
+ */
+var SeaweedFSService = require('./lib/seaweedfs-service');
+
+/**
+ * Initialize storage component.
+ */
+exports.initialize = function(dataSource, callback) {
+  var settings = dataSource.settings || {};
+
+  var connector = new SeaweedFSService(settings);
+  dataSource.connector = connector;
+  dataSource.connector.dataSource = dataSource;
+
+  connector.DataAccessObject = function () {};
+
+  for (var m in SeaweedFSService.prototype) {
+    var method = SeaweedFSService.prototype[m];
+    if (typeof method === 'function') {
+      connector.DataAccessObject[m] = method.bind(connector);
+      for (var k in method) {
+        connector.DataAccessObject[m][k] = method[k];
+      }
+    }
+  }
+
+  connector.define = function(model, properties, settings) {};
+
+  if (callback) {
+    dataSource.connector.connect(callback);
+  }
+};
